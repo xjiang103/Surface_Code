9bfe289d043b56ec4dc53c9ddc5dde2c7425ae3f
Xiaoyu Jiang <xjiang@DESKTOP-JJKTTKE.localdomain>
2022-04-25 12:17:30 -0500

    22_0425

---
+++ mqgpaper/CZk/CZk_sp/czkplot_db.py
@@ -0,0 +1,28 @@
+from tabulate import tabulate
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams.update({'font.size': 12})
+plt.rcParams["figure.figsize"] = (3.375,2.375)
+##content=[["qinrui",26,'physics'],["xiaoyu",28,'math']]
+##table=tabulate(content,headers=['name','age','major'])
+##print(table)
+
+
+dbarr=[0,0.05,0.1,0.15,0.2,0.25]
+farr2=[0.9988230567464242,0.9986701384551281,0.9975164959274561,0.9955716490752029,
+       0.9930156664737482,0.9899933136907312]
+farr3=[0.9928250952361852 ,0.9925117275367986,0.9920964528061972,0.9916012908094677,0.991044624724391 ,0.9898036291091025]
+farr4=[0.9995607107055559,0.9995580052405,0.999549236136917,0.9995398231406177,
+       0.9995186761448172,0.99928468405584069]
+
+plt.plot(dbarr,farr2,'o-',label="2")
+plt.plot(dbarr,farr3,'o-',label="3")
+#plt.plot(dbarr4,farr4,'o-',label="4")
+plt.legend(loc='lower right',  ncol=3, columnspacing=1.5)
+plt.ylim([0.9850,1])
+plt.xlabel("T-T coupling/C-T coupling")
+plt.ylabel("Fidelity")
+#plt.title("F vs min(Β)")
+
+filestr='czk_dbplot.png'
+plt.savefig(filestr, bbox_inches='tight',dpi=100)
